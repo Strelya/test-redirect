c8f54601eb1ca0d83e1c9263c2627e8c694c15d6
Strelya <ukrsite@mail.ru>
2018-05-05 21:13:59 +0300

    check upd

--- check.php
+++ check.php
@@ -5,3 +5,19 @@
  * Date: 02.05.2018
  * Time: 13:08
  */
+stream_context_set_default( [
+    'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+    ],
+]);
+$sumb = array("www.", "http:", "https:", "/");
+$domain_clear = str_replace($sumb, "", htmlspecialchars($_POST['Domain']));
+$domain_plus = array("http://", "https://", "http://www.", "https://www.");//
+foreach ($domain_plus as $value) {
+
+    $url = $value.$domain_clear;
+    $code = get_headers($url, 1);
+    echo $url." - ".$code[0];
+    echo "<br/>";
+};
