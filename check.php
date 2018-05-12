4839ab4695987bcf31732a5f48ec26ee52d6cab8
Strelya <ukrsite@mail.ru>
2018-05-12 13:25:25 +0300

    Processing with switch

--- check.php
+++ check.php
@@ -14,10 +14,44 @@ stream_context_set_default( [
 $sumb = array("www.", "http:", "https:", "/");
 $domain_clear = str_replace($sumb, "", htmlspecialchars($_POST['Domain']));
 $domain_plus = array("http://", "https://", "http://www.", "https://www.");//
+$http_header=0;//Надо подсчитать сколько 200/401/500 и выдавать рекомендацию.
 foreach ($domain_plus as $value) {
 
     $url = $value.$domain_clear;
+    //Alternative method, file_get_contents
+    //file_get_contents($url, null, stream_context_create(['http' => ['method' => 'GET']]));
+    //$code = $http_response_header[0];
+    //echo $url." - ".$code;
     $code = get_headers($url, 1);
-    echo $url." - ".$code[0];
-    echo "<br/>";
+
+    switch ($code[0]) {
+        case 'HTTP/1.1 200 OK':
+            $http_header++;
+            echo $url." - 200";
+            echo "<br/>";
+            break;
+        case 'HTTP/1.1 301 Moved Permanently':
+            echo $url." - 301";
+            echo "<br/>";
+            break;
+        case 'HTTP/1.1 401 Unauthorized':
+            echo $url." - ".$code[0];
+            echo "<br/>";
+            break;
+        case '502':// Запрос не обработается - доработать через if
+            echo $url." - ".$code[0];
+            echo "<br/>";
+            break;
+        case '500'://Создать ошибку, протестить
+            echo $url." - ".$code[0];
+            echo "<br/>";
+            break;
+    }
 };
+
+if ($http_header==1){
+    echo "Все отлично, главное зеркало настроено";
+}
+else {
+    echo "Необходимо настроить редирект на одно зеркало";
+}
